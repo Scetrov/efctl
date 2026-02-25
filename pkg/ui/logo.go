83885f47df7f505dbfee1b107af7b18d7ac6120f
Scetrov <git@scetrov.live>
2026-02-25 23:09:44 +0000

    Update startup banner with large cyan-to-orange gradient figlet font

--- pkg/ui/logo.go
+++ pkg/ui/logo.go
@@ -1,27 +1,33 @@
 package ui
 
-import "github.com/pterm/pterm"
+import (
+	"strings"
 
-const LogoASCII = `
-         /\
-        //\\
-       //  \\
-      //____\\
-      |      |
-      |      |
-     /|      |\
-    //|______|\ \
-      /  \/  \
-     /        \
-
-     /==========\
-    /            \
-   /    efctl     \
-   \              /
-    \            /
-     \==========/
-`
+	"github.com/pterm/pterm"
+	"github.com/pterm/pterm/putils"
+)
 
 func PrintBanner() {
-	pterm.DefaultCenter.Println(pterm.NewRGB(255, 116, 0).Sprint(LogoASCII))
+	cyan := pterm.NewRGB(0, 255, 255)
+	orange := pterm.NewRGB(255, 116, 0)
+
+	raw, _ := pterm.DefaultBigText.WithLetters(putils.LettersFromString("efctl")).Srender()
+
+	lines := strings.Split(raw, "\n")
+	for _, line := range lines {
+		clean := pterm.RemoveColorFromString(line)
+		var gradStr string
+		runes := []rune(clean)
+		for i, r := range runes {
+			// Prevent divide by zero or out of bounds fading
+			maxLen := len(runes)
+			if maxLen == 0 {
+				maxLen = 1
+			}
+			c := cyan.Fade(0, float32(maxLen), float32(i), orange)
+			gradStr += c.Sprint(string(r))
+		}
+		pterm.Println(gradStr)
+	}
+	pterm.Println()
 }
